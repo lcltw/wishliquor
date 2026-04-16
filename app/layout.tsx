8611888956a5b7c24edf4e6f87b125fc3c2f386d
lcltw <cltwclaw@gmail.com>
2026-04-16 13:30:47 +0800

    Add dir=ltr for Speed Insights compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" dir="ltr">
       <body className="min-h-screen">
         {children}
         <SpeedInsights />
